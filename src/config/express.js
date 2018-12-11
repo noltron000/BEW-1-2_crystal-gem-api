b98cedf258c44e5cdcaf4f4dc23b5d36486d8229
Nolan Kovacik <noltron000@gmail.com>
2018-12-10 23:22:00 -0800

    🔧 fixing up starter scripts

--- src/config/express.js
+++ src/config/express.js
@@ -1,20 +1,34 @@
+// require middleware
 const express = require('express');
+const exprHBS = require('express-handlebars');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const routes = require('../index.route');
-
+// import other scripts
+const routes = require('../index-route.js');
+const gem = require('../server/gem/gem-controller.js');
+const fusion = require('../server/fusion/fusion-controller.js');
+// define app
 const app = express();
 
-
+// use body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Enable CORS - Cross Origin Resource Sharing.
 app.use(cors());
 
-// Mount all routes on /api path.
-app.use('/api', routes);
+// set up handlebars
+app.engine('.hbs', exprHBS({ extname: '.hbs', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
 
-// #TODO: Additional non-API routes go here.
+// start up basic routes
+app.get('/', (req, res) => {
+	res.render('home.hbs');
+});
+
+// Mount all other routes
+app.use('/api', routes);
+app.use('/gem', gem);
+app.use('/fusion', fusion);
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-// config should be imported before importing any other file
-const config = require('./config/config');
-const app = require('./config/express');
-const exprHBS = require('express-handlebars');
+// mongoose and util come first
 const mongoose = require('mongoose');
 const util = require('util');
+// config & app should be imported before any other file
+const config = require('./config/config');
+const app = require('./config/express');
 const debug = require('debug')('auth-api-starterpack:index');
 
-// get promise
 mongoose.Promise = Promise;
 
 // connect to mongo db
@@ -28,9 +27,7 @@ if (config.mongooseDebug) {
 	});
 }
 
-// set up handlebars
-app.engine('.hbs', exprHBS({ extname: '.hbs', defaultLayout: 'main' }));
-app.set('view engine', 'hbs');
+// # TODO: Any additional config changes belong here.
 
 // module.parent check is required to support mocha watch
 // src: https://github.com/mochajs/mocha/issues/1912
@@ -40,19 +37,6 @@ if (!module.parent) {
 		console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
 	});
 }
-
-// start up basic routes
-app.get('/', (req, res) => {
-	res.render('home.hbs');
-});
-
-// require other routes
-const gem = require('./server/gem/gem-controller.js');
-const fusion = require('./server/fusion/fusion-controller.js');
-
-// use the routes
-app.use('/gem', gem);
-app.use('/fusion', fusion);
 
 // // get data
 // const data = require('./data/flamewarz-db.js');
