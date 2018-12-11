bbf292e60beda06a9653e8a03b9baad1c6da9794
Nolan Kovacik <noltron000@gmail.com>
2018-12-10 22:43:25 -0800

    adding server files

---
+++ src/config/data.js
@@ -0,0 +1,18 @@
+/* Mongoose Connection */
+const mongoose = require('mongoose');
+const assert = require('assert');
+
+const url = 'mongodb://localhost/gem-api';
+mongoose.Promise = global.Promise;
+mongoose.connect(
+	url,
+	{ useNewUrlParser: true },
+	(err) => {
+		assert.equal(null, err);
+		console.log('Connected successfully to database');
+	}
+);
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
+mongoose.set('debug', true);
+
+module.exports = mongoose.connection;
