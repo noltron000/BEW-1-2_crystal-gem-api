bddf2a4ec84e65affe11eb796a27b341b6b52479
Nolan Kovacik <noltron000@gmail.com>
2018-12-12 20:43:32 -0800

    editing the listen server 👂

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (config.mongooseDebug) {
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
 	// listen on port config.port
-	app.listen(config.port, () => {
+	app.listen(process.env.PORT || config.port, () => {
 		console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
 	});
 }
